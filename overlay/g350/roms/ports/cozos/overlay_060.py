a7c320a0c7bc50549bf64dadded5711ad54efcc5
UnknownApe1 <327497807+UnknownApe1@users.noreply.github.com>
2026-09-17 12:58:00 -0400

    Add CozOS 0.6.0 overlay compatibility wrapper

---
+++ overlay/g350/roms/ports/cozos/overlay_060.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Run the hardware-tested CozOS overlay helper with the 0.6.0 release identity."""
+import sys
+import overlay as legacy
+legacy.VERSION='0.6.0'
+if __name__=='__main__':
+    sys.exit(legacy.main())
